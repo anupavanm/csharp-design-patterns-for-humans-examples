b627c954c2a92d529bc50bf9a246c489df811aa2
anupavanm <anupavanm.muddana@ajboggs.com>
2018-04-10 13:29:58 -0400

    Singleton Pattern example

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+  public class President
+  {
+    static President instance;
+    // Private constructor
+    private President()
+    {
+      //Hiding the Constructor
+    }
+
+    // Public constructor
+    public static President get_instance()
+    {
+      if (instance == null) {
+        instance = new President();
+      }
+      return instance;
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      President a = President.get_instance();
+      President b = President.get_instance();
+
+      Console.WriteLine((a==b).ToString());
+      Console.ReadLine();
+    }
+  }
+}
