7155825ce4e4b4ef663d1ab0e856f8f720b42e43
anupavanm <anupavanm.muddana@ajboggs.com>
2018-04-10 19:30:06 -0400

    Adapter pattern example

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+  interface ILion
+  {
+    void Roar();
+  }
+
+  class AfricanLion : ILion
+  {
+    public void Roar()
+    {
+
+    }
+  }
+
+  class AsiaLion : ILion
+  {
+    public void Roar()
+    {
+
+    }
+  }
+
+  // This needs to be added to the game
+  class WildDog
+  {
+    public void bark()
+    {
+    }
+  }
+
+  // Adapter around wild dog to make it compatible with our game
+  class WildDogAdapter : ILion
+  {
+    private WildDog mDog;
+    public WildDogAdapter(WildDog dog)
+    {
+      this.mDog = dog;
+    }
+    public void Roar()
+    {
+      mDog.bark();
+    }
+  }
+
+  class Hunter
+  {
+    public void Hunt(ILion lion)
+    {
+
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var wildDog = new WildDog();
+      var wildDogAdapter = new WildDogAdapter(wildDog);
+
+      var hunter = new Hunter();
+      hunter.Hunt(wildDogAdapter);
+
+      Console.ReadLine();
+    }
+  }
+}
