4a217bb3f37065ff543d572a1e7b521ad1629a7e
anupavanm <anupavanm@gmail.com>
2018-04-14 14:10:07 -0400

    Facede pattern example

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace FacadePattern
+{
+class Computer
+{
+  public void GetElectricShock()
+  {
+    Console.Write("Ouch!");
+  }
+
+  public void MakeSound()
+  {
+    Console.Write("Beep beep!");
+  }
+
+  public void ShowLoadingScreen()
+  {
+    Console.Write("Loading..");
+  }
+
+  public void Bam()
+  {
+    Console.Write("Ready to be used!");
+  }
+
+  public void CloseEverything()
+  {
+    Console.Write("Bup bup bup buzzzz!");
+  }
+
+  public void Sooth()
+  {
+    Console.Write("Zzzzz");
+  }
+
+  public void PullCurrent()
+  {
+    Console.Write("Haaah!");
+  }
+}
+
+class ComputerFacade
+{
+  private readonly Computer mComputer;
+
+  public ComputerFacade(Computer computer)
+  {
+    this.mComputer = computer ?? throw new ArgumentNullException("computer", "computer cannot be null");
+  }
+
+  public void TurnOn()
+  {
+    mComputer.GetElectricShock();
+    mComputer.MakeSound();
+    mComputer.ShowLoadingScreen();
+    mComputer.Bam();
+  }
+
+  public void TurnOff()
+  {
+    mComputer.CloseEverything();
+    mComputer.PullCurrent();
+    mComputer.Sooth();
+  }
+}
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var computer = new ComputerFacade(new Computer());
+      computer.TurnOn(); // Ouch! Beep beep! Loading.. Ready to be used!
+      Console.WriteLine();
+      computer.TurnOff();  // Bup bup buzzz! Haah! Zzzzz
+      Console.ReadLine();
+    }
+  }
+}
