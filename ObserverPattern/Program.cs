fefa85e88478faa63c160d006647a87f09b0e5fb
anupavanm <anupavanm@gmail.com>
2018-04-15 13:32:12 -0400

    observer pattern example

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+class JobPost
+{
+  public string Title { get; private set; }
+
+  public JobPost(string title)
+  {
+    Title = title;
+  }
+}
+class JobSeeker : IObserver<JobPost>
+{
+  public string Name { get; private set; }
+
+  public JobSeeker(string name)
+  {
+    Name = name;
+  }
+
+  //Method is not being called by JobPostings class currently
+  public void OnCompleted()
+  {
+    //No Implementation
+  }
+
+  //Method is not being called by JobPostings class currently
+  public void OnError(Exception error)
+  {
+    //No Implementation
+  }
+
+  public void OnNext(JobPost value)
+  {
+    Console.WriteLine("Hi {0} ! New job posted: {1}", Name, value.Title);
+  }
+}
+
+class JobPostings : IObservable<JobPost>
+{
+  private List<IObserver<JobPost>> mObservers;
+  private List<JobPost> mJobPostings;
+
+  public JobPostings()
+  {
+    mObservers = new List<IObserver<JobPost>>();
+    mJobPostings = new List<JobPost>();
+  }
+
+  public IDisposable Subscribe(IObserver<JobPost> observer)
+  {
+    // Check whether observer is already registered. If not, add it
+    if (!mObservers.Contains(observer))
+    {
+      mObservers.Add(observer);
+    }
+    return new Unsubscriber<JobPost>(mObservers, observer);
+  }
+
+  private void Notify(JobPost jobPost)
+  {
+    foreach(var observer in mObservers)
+    {
+      observer.OnNext(jobPost);
+    }
+  }
+
+  public void AddJob(JobPost jobPost)
+  {
+    mJobPostings.Add(jobPost);
+    Notify(jobPost);
+  }
+
+}
+
+internal class Unsubscriber<JobPost> : IDisposable
+{
+  private List<IObserver<JobPost>> mObservers;
+  private IObserver<JobPost> mObserver;
+
+  internal Unsubscriber(List<IObserver<JobPost>> observers, IObserver<JobPost> observer)
+  {
+    this.mObservers = observers;
+    this.mObserver = observer;
+  }
+
+  public void Dispose()
+  {
+    if (mObservers.Contains(mObserver))
+      mObservers.Remove(mObserver);
+  }
+}
+
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+//Create Subscribers
+var johnDoe = new JobSeeker("John Doe");
+var janeDoe = new JobSeeker("Jane Doe");
+
+//Create publisher and attch subscribers
+var jobPostings = new JobPostings();
+jobPostings.Subscribe(johnDoe);
+jobPostings.Subscribe(janeDoe);
+
+//Add a new job and see if subscribers get notified
+jobPostings.AddJob(new JobPost("Software Engineer"));
+
+//Output
+// Hi John Doe! New job posted: Software Engineer
+// Hi Jane Doe! New job posted: Software Engineer
+
+Console.ReadLine();
+    }
+  }
+}
