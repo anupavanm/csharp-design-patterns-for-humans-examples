dc36677e09f5ecfc67fc6c41bef4df222b41af3b
anupavanm <anupavanm@gmail.com>
2018-04-15 13:51:01 -0400

    visitor pattern example

---
+++ VisitorPattern/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace VisitorPattern
+{
+// Visitee
+interface IAnimal
+{
+  void Accept(IAnimalOperation operation);
+}
+
+// Visitor
+interface IAnimalOperation
+{
+  void VisitMonkey(Monkey monkey);
+  void VisitLion(Lion lion);
+  void VisitDolphin(Dolphin dolphin);
+}
+
+class Monkey : IAnimal
+{
+  public void Shout()
+  {
+    Console.WriteLine("Oooh o aa aa!");
+  }
+
+  public void Accept(IAnimalOperation operation)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+class Lion : IAnimal
+{
+  public void Roar()
+  {
+    Console.WriteLine("Roaar!");
+  }
+
+  public void Accept(IAnimalOperation operation)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+class Dolphin : IAnimal
+{
+  public void Speak()
+  {
+    Console.WriteLine("Tuut tittu tuutt!");
+  }
+
+  public void Accept(IAnimalOperation operation)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+class Speak : IAnimalOperation
+{
+  public void VisitDolphin(Dolphin dolphin)
+  {
+    dolphin.Speak();
+  }
+
+  public void VisitLion(Lion lion)
+  {
+    lion.Roar();
+  }
+
+  public void VisitMonkey(Monkey monkey)
+  {
+    monkey.Shout();
+  }
+}
+
+class Jump : IAnimalOperation
+{
+  public void VisitDolphin(Dolphin dolphin)
+  {
+    Console.WriteLine("Jumped 20 feet high! on to the tree!");
+  }
+
+  public void VisitLion(Lion lion)
+  {
+    Console.WriteLine("Jumped 7 feet! Back on the ground!");
+  }
+
+  public void VisitMonkey(Monkey monkey)
+  {
+    Console.WriteLine("Walked on water a little and disappeared!");
+  }
+}
+
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+var monkey = new Monkey();
+var lion = new Lion();
+var dolphin = new Dolphin();
+
+var speak = new Speak();
+
+monkey.Accept(speak);    // Ooh oo aa aa!    
+lion.Accept(speak);      // Roaaar!
+dolphin.Accept(speak);   // Tuut tutt tuutt!
+
+var jump = new Jump();
+
+monkey.Accept(speak);   // Ooh oo aa aa!
+monkey.Accept(jump);    // Jumped 20 feet high! on to the tree!
+
+lion.Accept(speak);     // Roaaar!
+lion.Accept(jump);      // Jumped 7 feet! Back on the ground!
+
+dolphin.Accept(speak);  // Tuut tutt tuutt!
+dolphin.Accept(jump);   // Walked on water a little and disappeared
+
+      Console.ReadLine();
+    }
+  }
+}
