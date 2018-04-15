4005efd0931432dbf0dbc8642a7b088434f6c603
anupavanm <anupavanm@gmail.com>
2018-04-15 16:05:28 -0400

    prototype pattern example

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+class Sheep
+{
+  public string Name { get; set; }
+
+  public string Category { get; set; }
+
+  public Sheep(string name, string category)
+  {
+    Name = name;
+    Category = category;
+  }
+
+  public Sheep Clone()
+  {
+    return MemberwiseClone() as Sheep;
+  }
+}
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var original = new Sheep("Jolly", "Mountain Sheep");
+      Console.WriteLine(original.Name); // Jolly
+      Console.WriteLine(original.Category); // Mountain Sheep
+
+      var cloned = original.Clone();
+      cloned.Name = "Dolly";
+      Console.WriteLine(cloned.Name); // Dolly
+      Console.WriteLine(cloned.Category); // Mountain Sheep
+      Console.WriteLine(original.Name); // Dolly
+
+      Console.ReadLine();
+    }
+  }
+}
