65d64aa538a60951020392cd8db6f24e396840e4
anupavanm <anupavanm@gmail.com>
2018-04-15 14:05:02 -0400

    Strategy pattern example

---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyPattern
+{
+  interface ISortStrategy
+  {
+    List<int> Sort(List<int> dataset);
+  }
+
+  class BubbleSortStrategy : ISortStrategy
+  {
+    public List<int> Sort(List<int> dataset)
+    {
+      Console.WriteLine("Sorting using Bubble Sort !");
+      return dataset;
+    }
+  }
+
+  class QuickSortStrategy : ISortStrategy
+  {
+    public List<int> Sort(List<int> dataset)
+    {
+      Console.WriteLine("Sorting using Quick Sort !");
+      return dataset;
+    }
+  }
+
+  class Sorter
+  {
+    private readonly ISortStrategy mSorter;
+
+    public Sorter(ISortStrategy sorter)
+    {
+      mSorter = sorter;
+    }
+
+    public List<int> Sort(List<int> unSortedList)
+    {
+      return mSorter.Sort(unSortedList);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var unSortedList = new List<int> { 1, 10, 2, 16, 19 };
+
+      var sorter = new Sorter(new QuickSortStrategy());
+      sorter.Sort(unSortedList); // // Output : Sorting using Bubble Sort !
+
+      sorter = new Sorter(new QuickSortStrategy());
+      sorter.Sort(unSortedList); // // Output : Sorting using Quick Sort !
+
+      Console.ReadLine();
+    }
+  }
+}
