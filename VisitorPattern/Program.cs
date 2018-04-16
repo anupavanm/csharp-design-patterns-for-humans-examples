d3c6a3e978db2a8e4a524d845f5d37fa9fb52942
anupavanm <anupavanm@gmail.com>
2018-04-16 19:04:40 -0400

    visitor pattern changes

--- VisitorPattern/Program.cs
+++ VisitorPattern/Program.cs
@@ -2,120 +2,120 @@
 
 namespace VisitorPattern
 {
-  // Visitee
-  interface IAnimal
+// Visitee
+interface IAnimal
+{
+  void Accept(IAnimalOperation operation);
+}
+
+// Visitor
+interface IAnimalOperation
+{
+  void VisitMonkey(Monkey monkey);
+  void VisitLion(Lion lion);
+  void VisitDolphin(Dolphin dolphin);
+}
+
+class Monkey : IAnimal
+{
+  public void Shout()
   {
-    void Accept(IAnimalOperation operation);
+    Console.WriteLine("Oooh o aa aa!");
   }
 
-  // Visitor
-  interface IAnimalOperation
+  public void Accept(IAnimalOperation operation)
   {
-    void VisitMonkey(Monkey monkey);
-    void VisitLion(Lion lion);
-    void VisitDolphin(Dolphin dolphin);
+      operation.VisitMonkey(this);
+  }
+}
+
+class Lion : IAnimal
+{
+  public void Roar()
+  {
+    Console.WriteLine("Roaar!");
   }
 
-  class Monkey : IAnimal
+  public void Accept(IAnimalOperation operation)
   {
-    public void Shout()
-    {
-      Console.WriteLine("Oooh o aa aa!");
-    }
+      operation.VisitLion(this);
+  }
+}
 
-    public void Accept(IAnimalOperation operation)
-    {
-      throw new NotImplementedException();
-    }
+class Dolphin : IAnimal
+{
+  public void Speak()
+  {
+    Console.WriteLine("Tuut tittu tuutt!");
   }
 
-  class Lion : IAnimal
+  public void Accept(IAnimalOperation operation)
   {
-    public void Roar()
-    {
-      Console.WriteLine("Roaar!");
-    }
+      operation.VisitDolphin(this);
+  }
+}
 
-    public void Accept(IAnimalOperation operation)
-    {
-      throw new NotImplementedException();
-    }
+class Speak : IAnimalOperation
+{
+  public void VisitDolphin(Dolphin dolphin)
+  {
+    dolphin.Speak();
   }
 
-  class Dolphin : IAnimal
+  public void VisitLion(Lion lion)
   {
-    public void Speak()
-    {
-      Console.WriteLine("Tuut tittu tuutt!");
-    }
-
-    public void Accept(IAnimalOperation operation)
-    {
-      throw new NotImplementedException();
-    }
+    lion.Roar();
   }
 
-  class Speak : IAnimalOperation
+  public void VisitMonkey(Monkey monkey)
   {
-    public void VisitDolphin(Dolphin dolphin)
-    {
-      dolphin.Speak();
-    }
-
-    public void VisitLion(Lion lion)
-    {
-      lion.Roar();
-    }
-
-    public void VisitMonkey(Monkey monkey)
-    {
-      monkey.Shout();
-    }
+    monkey.Shout();
   }
+}
 
-  class Jump : IAnimalOperation
+class Jump : IAnimalOperation
+{
+  public void VisitDolphin(Dolphin dolphin)
   {
-    public void VisitDolphin(Dolphin dolphin)
-    {
       Console.WriteLine("Walked on water a little and disappeared!");
     }
 
-    public void VisitLion(Lion lion)
-    {
-      Console.WriteLine("Jumped 7 feet! Back on the ground!");
-    }
+  public void VisitLion(Lion lion)
+  {
+    Console.WriteLine("Jumped 7 feet! Back on the ground!");
+  }
 
-    public void VisitMonkey(Monkey monkey)
-    {
+  public void VisitMonkey(Monkey monkey)
+  {
       Console.WriteLine("Jumped 20 feet high! on to the tree!");
     }
-  }
+}
 
 
   class Program
   {
     static void Main(string[] args)
     {
-      var monkey = new Monkey();
-      var lion = new Lion();
-      var dolphin = new Dolphin();
+var monkey = new Monkey();
+var lion = new Lion();
+var dolphin = new Dolphin();
 
-      var speak = new Speak();
+var speak = new Speak();
 
-      monkey.Accept(speak);    // Ooh oo aa aa!    
-      lion.Accept(speak);      // Roaaar!
-      dolphin.Accept(speak);   // Tuut tutt tuutt!
+monkey.Accept(speak);    // Ooh oo aa aa!    
+lion.Accept(speak);      // Roaaar!
+dolphin.Accept(speak);   // Tuut tutt tuutt!
 
-      var jump = new Jump();
+var jump = new Jump();
 
-      monkey.Accept(speak);   // Ooh oo aa aa!
-      monkey.Accept(jump);    // Jumped 20 feet high! on to the tree!
+monkey.Accept(speak);   // Ooh oo aa aa!
+monkey.Accept(jump);    // Jumped 20 feet high! on to the tree!
 
-      lion.Accept(speak);     // Roaaar!
-      lion.Accept(jump);      // Jumped 7 feet! Back on the ground!
+lion.Accept(speak);     // Roaaar!
+lion.Accept(jump);      // Jumped 7 feet! Back on the ground!
 
-      dolphin.Accept(speak);  // Tuut tutt tuutt!
-      dolphin.Accept(jump);   // Walked on water a little and disappeared
+dolphin.Accept(speak);  // Tuut tutt tuutt!
+dolphin.Accept(jump);   // Walked on water a little and disappeared
 
       Console.ReadLine();
     }
